f65f8e1efa2a5fce40195e4482dca25789965f2e
nemiko007 <otoha.youjopero2@gmail.com>
2023-10-20 17:34:08 +0900

    imgの位置の変更

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -5,10 +5,10 @@ export default function Aside(){
             <div className="photo">
                 <div>
                     <a href="favorite-1.jpg"></a><img src="favorite-1_th.jpg" alt="Small black bird, black claws, long black slender beak, links to larger version of the image"></img>
-                    <a href="favorite-2.jpg"></a><img src="favorite-2_th.jpg" alt="Top half of a pretty bird with bright blue plumage on neck, light colored beak, blue headdress, links to larger version of the image"></img>
+                    <a href="favorite-3.jpg"></a><img src="favorite-3_th.jpg" alt="Top half of a large bird with white plumage, very long curved narrow light colored break, links to larger version of the image"></img>
                 </div>
                 <div>
-                    <a href="favorite-3.jpg"></a><img src="favorite-3_th.jpg" alt="Top half of a large bird with white plumage, very long curved narrow light colored break, links to larger version of the image"></img>
+                    <a href="favorite-2.jpg"></a><img src="favorite-2_th.jpg" alt="Top half of a pretty bird with bright blue plumage on neck, light colored beak, blue headdress, links to larger version of the image"></img>
                     <a href="favorite-4.jpg"></a><img src="favorite-4_th.jpg" alt="Large bird, mostly white plumage with black plumage on back and rear, long straight white beak, links to larger version of the image"></img>
                 </div>
             </div>
